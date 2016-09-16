6d62ee38779f807f8576d08964c44e11de750fdb
hemesh-patel <hemeshpatel@hotmail.co.uk>
2016-09-16 15:47:01 +0100

    update, group and average

--- Weather/cleaning_weather.py
+++ Weather/cleaning_weather.py
@@ -2,55 +2,39 @@ import json
 from pandas.io.json import json_normalize
 from pandas import DataFrame, Series
 import pyodbc
+import numpy as np
 
 cnx = pyodbc.connect('DSN=Kubrick')
 cursor = cnx.cursor()
 
 cursor.execute('select * from [dbo].[weather]')
 row = cursor.fetchall()
-print type(row)
 
-info = []
+date = []
+data = []
 for i in row:
-    r = json.loads(i)
-    info.append(r)
-print type(info)
+    row1 = i
+    rowx = row1[1]
+    r = json.loads(rowx)
+    data.append(r)
+    date.append(r['SiteRep']['DV']['dataDate'][:10])#  This extracts the date of when the information was collected
+
+extraction_date = date[0]
+path = data[0]['SiteRep']['DV']['Location']['Period']#  This is the path to the dates, times and weather info in a list format. Each element in list is a different day.
+
+result = json_normalize(path, 'Rep', ['value'])
 
 
-
-#t1 = Series(info, name='SiteRep')
-#print t1
-#for i in row:
-#    print i
-#    with open('outputfilename.json', 'wb') as outfile:
-#        json.dump(i, outfile)
-
-#weather_file = 'outputfilename.json'
-#with open(weather_file) as fhand:
-#    data = json.load(fhand)
-#    print type(dict(data))
-#print dict(data)
-#print type(data)
-#path = data['SiteRep']['DV']['Location']['Period'] # This is the path to the dates, times and weather info in a list format. Each element in list is a different day.
-#print path
-#result = json_normalize(path, 'Rep', ['value'])
-#print result
-
-#day = path[0]['Rep'][0]['$'] # This gives weather info on the whole day
-#print day
-
-#print type(result)
-#df2 = DataFrame(result, columns =['$','Pp', 'W', 'value'])
-#df2['index'] = range(1,len(df2)+1)
+#df2 = DataFrame(result, columns=['$', 'Pp', 'value'])
 #print df2
-#df3 = df2.set_index('index').T.to_dict('list')
-#print df3
-
-#df4 = DataFrame(result, columns =['$', 'W', 'value'])
-#df4['index'] = range(1,len(df2)+1)
-#print df4
 
 
+df2 = DataFrame(result, columns=['value', 'Pp'])
+df2[['Pp']] = df2[['Pp']].astype(int)
+print df2
+#Trying to change the data type of pp to int
+#df3 = DataFrame(df2['value'], df2['Pp'].astype(int))
+#print df3[['Pp', 'value']]
 
-
-
+group = df2.groupby('value')['Pp'].mean()
+print group
