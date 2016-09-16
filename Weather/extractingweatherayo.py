749c52f72dd1bae664bfbef0bf727ef3c52c28a9
ayogitz <Manga987>
2016-09-16 12:33:25 +0100

    hjfg

---
+++ Weather/extractingweatherayo.py
@@ -0,0 +1,57 @@
+import json
+from pandas.io.json import json_normalize
+from pandas import DataFrame, Series
+import pyodbc
+
+cnx = pyodbc.connect('DSN=Kubrick')
+cursor = cnx.cursor()
+
+cursor.execute('select * from [dbo].[weather]')
+row = cursor.fetchall()
+dates = []
+info = []
+#print len(rows)
+for i in row:
+    row1 = i
+    rowx = row1[1]
+    r = json.loads(rowx)
+    print r['SiteRep']
+  # tweet.append(r['text'])
+  # dates.append(r['created_at'])
+  # ids.append(r['id'])
+
+
+
+
+#t1 = Series(info, name='SiteRep')
+#print t1
+#for i in row:
+#    print i
+#    with open('outputfilename.json', 'wb') as outfile:
+#        json.dump(i, outfile)
+
+#weather_file = 'outputfilename.json'
+#with open(weather_file) as fhand:
+#    data = json.load(fhand)
+#    print type(dict(data))
+#print dict(data)
+#print type(data)
+#path = data['SiteRep']['DV']['Location']['Period'] # This is the path to the dates, times and weather info in a list format. Each element in list is a different day.
+#print path
+#result = json_normalize(path, 'Rep', ['value'])
+#print result
+
+#day = path[0]['Rep'][0]['$'] # This gives weather info on the whole day
+#print day
+
+#print type(result)
+#df2 = DataFrame(result, columns =['$','Pp', 'W', 'value'])
+#df2['index'] = range(1,len(df2)+1)
+#print df2
+#df3 = df2.set_index('index').T.to_dict('list')
+#print df3
+
+#df4 = DataFrame(result, columns =['$', 'W', 'value'])
+#df4['index'] = range(1,len(df2)+1)
+#print df4
+
