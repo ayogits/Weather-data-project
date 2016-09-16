e466d56ad09e838977a073c1412c5b6147a86f70
ayogitz <Manga987>
2016-09-16 10:59:23 +0100

    hhh

---
+++ Weather/extratcingweatherinsertayorrrrrrrrr.py
@@ -0,0 +1,23 @@
+import json
+import urllib2
+from bs4 import BeautifulSoup
+import pyodbc
+
+api_key = 'f0f01451-af09-4943-a626-0dd57bcf533d'
+
+weather = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3316?res=3hourly&key=' + api_key
+page = urllib2.urlopen(weather)
+soup = BeautifulSoup((page), "lxml")
+j_file = json.loads(str(soup.p.string))
+rep_data = json.dumps(j_file)
+print rep_data
+database_name = 'Kubrick'
+your_table = 'weather'
+
+cnx = pyodbc.connect('DSN='+ database_name)
+cursor = cnx.cursor()
+
+insertcmd = "insert into dbo." + your_table + "(Info) values (?)"
+
+cursor.execute(insertcmd, rep_data)
+cnx.commit()
