3b68bccd9aebeb77e33f45800b930fcb206fa480
hemesh-patel <hemeshpatel@hotmail.co.uk>
2016-09-20 14:47:42 +0100

    sql to mongo

--- Twitter/cleantwitter.py
+++ Twitter/cleantwitter.py
@@ -1,26 +1,43 @@
 import pyodbc
 import json
 import pandas
+import pymongo
+from pymongo import MongoClient
+
 cnx = pyodbc.connect('DSN=Kubrick')
 cursor = cnx.cursor()
 
-cursor.execute("select * from [dbo].[tweets]")
+cursor.execute("select * from [dbo].[twitter]")
 rows = cursor.fetchall()
-tweet=[]
-dates = []
-ids = []
-for i in rows:
-    row1 = i
-    rowx = row1[1]
-    r = json.loads(rowx)
-    tweet.append(r['text'])
-    dates.append(r['created_at'])
-    ids.append(r['id'])
+tweets = []
+for row in rows:
+    tweets.append(json.loads(row[1]))
+print tweets
 
-t1 = pandas.Series(tweet, name='tweet')
-t2 = pandas.Series(dates, name='date')
-t3 = pandas.Series(ids, name='id')
-t4 = pandas.concat([t1, t2, t3], axis=1)  # s3 dataframe
-print t4['tweet']
+
+client = MongoClient('localhost', 27017)
+db = client['test']
+tweet_table = db['tweet_table']
+print tweet_table
+t = db.tweet_table
+for i in tweets:
+    t.insert(i)
+
+#tweet=[]
+##dates = []
+#ids = []
+#for i in rows:
+#    row1 = i
+#    rowx = row1[1]
+#    r = json.loads(rowx)
+#    tweet.append(r['text'])
+#    dates.append(r['created_at'])
+#    ids.append(r['id'])
+
+#t1 = pandas.Series(tweet, name='tweet')
+#t2 = pandas.Series(dates, name='date')
+#t3 = pandas.Series(ids, name='id')
+#t4 = pandas.concat([t1, t2, t3], axis=1)  # s3 dataframe
+#print t4['tweet']
 
 
