cef21225053918f1a96ce4c2ba2d334311d63e6c
hemesh-patel <hemeshpatel@hotmail.co.uk>
2016-09-20 17:11:33 +0100

    fds

---
+++ Weather/sql to mongo_weather.py
@@ -0,0 +1,32 @@
+import json
+import pyodbc
+from pymongo import MongoClient
+
+
+plot_api = 'il8uhkce0f'
+plot_username = 'hemeshpatel91'
+plotly.tools.set_credentials_file(username=plot_username, api_key=plot_api)
+
+cnx = pyodbc.connect('DSN=Kubrick')
+cursor = cnx.cursor()
+
+cursor.execute('select * from [dbo].[weather]') # change the table name
+row = cursor.fetchall()
+
+weather_dic = []
+lst= []
+for i in row:
+    line = str(i[1])
+    line1 = line.replace('$', 't')
+    weather_dic.append(line1)
+
+client = MongoClient('localhost', 27017)
+db = client['test']  # make sure we create a new collection
+weather_table = db['weather_table']
+w = db.weather_table
+
+for x in weather_dic:
+    w.insert(json.loads(x))
+
+
+
